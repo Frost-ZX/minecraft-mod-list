4036216dfe93c90b42a42ad08b87040613fe6c3f
Frost-ZX <frost-zx@foxmail.com>
2022-11-27 19:48:47 +0800

    build: 安装 vite-plugin-singlefile，支持打包为单文件

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,36 @@
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
 
   base: './',
-  plugins: [vue()],
+  plugins: [
+    viteSingleFile({
+      deleteInlinedFiles: true,
+      removeViteModuleLoader: false,
+      useRecommendedBuildConfig: false,
+    }),
+    vue(),
+  ],
   publicDir: 'public',
 
   build: {
+
     assetsDir: 'assets',
-    assetsInlineLimit: 0,
     outDir: 'dist',
+
+    // 适配 viteSingleFile
+    assetsInlineLimit: 104857600, // 100MB
+    cssCodeSplit: false,
+    reportCompressedSize: false,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+
   },
 
   server: {
